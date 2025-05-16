4aadb7ae4f91cd4bb08649f09dac3240af57d2f9
Your Name <you@example.com>
2025-05-16 16:24:45 +0200

    geojson

--- javascript.js
+++ javascript.js
@@ -6,3 +6,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
+
+  fetch('countries_count.geojson')
+        .then(response => response.json())
+        .then(data => {
+            L.geoJSON(data, {
+                style: {
+                    color: 'blue',
+                    weight: 2,
+                    fillOpacity: 0.3
+                }
+            }).addTo(map);
+        })
+        .catch(error => console.error('Error loading GeoJSON:', error));
